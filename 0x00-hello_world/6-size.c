5e09faf9c1f5fa28d72cb251a38392ea4becd007
Glitterz001 <pobadmus@gmail.com>
2022-09-08 13:23:35 -0700

    pelu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a lfloat: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
