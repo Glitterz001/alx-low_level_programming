949989c1f398f9fbb0dd48977cbcb71687333bda
Glitterz001 <pobadmus@gmail.com>
2022-09-15 02:35:16 -0700

    my secinnd commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
         int i;
 	/** Declearation */
         printf("Infinite loop incoming :(\n");
-
+	/** initialising */
         i = 0;
 
         while (i < 10)
         {
                 putchar(i);
         }
-
+	/** print the output */
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
