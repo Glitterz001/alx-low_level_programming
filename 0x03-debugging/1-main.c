db80df4b10d955401a9ee060c57cef9a9df90229
Glitterz001 <pobadmus@gmail.com>
2022-09-15 02:11:39 -0700

    my secinnd commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+	/** Declearation */
+
+        while (i < 10)
+
+	/** initialization */
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
