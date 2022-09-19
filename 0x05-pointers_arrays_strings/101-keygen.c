022304740f0c9c6dbd9df1d28055e2066e3d1bbc
Glitterz001 <pobadmus@gmail.com>
2022-09-19 14:42:20 -0700

    my third commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main retun valid password
+ * @valid password
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+		int a
+			a = valid password
+			printf("valid password"/n)
+		return (0)
+}
