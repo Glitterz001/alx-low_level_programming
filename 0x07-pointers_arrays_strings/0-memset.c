d5124eb0f2affe3f88148b784183b1f58b2ff893
Glitterz001 <pobadmus@gmail.com>
2022-09-26 12:19:27 -0700

    pelu

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-	int size = n; 
+	int size = n; /* only accept positive sizes */
 
 	if (size > 0)
 	{
@@ -23,3 +23,17 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
+
+/* pointer arithmetic version
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n;
+	if (size > 0)
+	{
+		int i;
+		for (i = 0; i < size; i++)
+		     *(s + i) = b;
+	}
+	return (s);
+}
+*/
