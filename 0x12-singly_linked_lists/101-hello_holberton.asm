a45aa83c56e1e2da1c159e9d08783eb25cd1687c
Glitterz001 <pobadmus@gmail.com>
2022-10-20 06:26:54 -0700

    my initial commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * execute_before_main - executes this function before main function
+ * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+ */
+
+void __attribute__ ((constructor)) execute_before_main()
+{
+	prinf("Hello,Holberton/n");
+}
