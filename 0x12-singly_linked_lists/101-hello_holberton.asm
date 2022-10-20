d3a240e4e26f25f2e5680dc4b5fdb482a743af7f
Glitterz001 <pobadmus@gmail.com>
2022-10-20 14:17:27 -0700

    sixth commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,23 @@
-; ----------------------------------------------------------------------------
-; helloholberton.asm
-;
-; This is a Win32 console program that writes "Hello, World" on one line and
-; then exits.  It needs to be linked with a C library.
-; ----------------------------------------------------------------------------
+global WinMain
+extern ExitProcess  ; external functions in system libraries 
+extern MessageBoxA
 
-    global  _main
-    extern  _printf
+section .data 
+title:  db 'Win64', 0
+msg:    db 'Hello, Holberton!', 0
 
-    section .text
-_main:
-    push    message
-    call    _printf
-    add     esp, 4
-    ret
-message:
-    db  'Hello,Holberton', 14, 0
+section .text
+WinMain:
+    sub rsp, 28h  
+    mov rcx, 0       ; hWnd = HWND_DESKTOP
+    lea rdx,[msg]    ; LPCSTR lpText
+    lea r8,[title]   ; LPCSTR lpCaption
+    mov r9d, 0       ; uType = MB_OK
+    call MessageBoxA
+    add rsp, 28h  
+
+    mov  ecx,eax
+    call ExitProcess
+
+    hlt     ; never here
+
